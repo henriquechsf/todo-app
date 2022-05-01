91cd9f8a6165237b0d099e88076e3fe1d66c83dc
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-01 01:57:22 -0300

    create menu for update fragment

--- app/src/main/java/tech/henriquedev/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/tech/henriquedev/todoapp/fragments/update/UpdateFragment.kt
@@ -1,10 +1,9 @@
 package tech.henriquedev.todoapp.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import tech.henriquedev.todoapp.R
 import tech.henriquedev.todoapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
@@ -15,7 +14,15 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Set Menu
+        setHasOptionsMenu(true)
+
         _binding = FragmentUpdateBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.udpate_fragment_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
