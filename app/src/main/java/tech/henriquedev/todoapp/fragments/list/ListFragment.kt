7e698dc3d707cac92068f7c75f8eba65bd6289f6
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-02 23:09:18 -0300

    display data in recyclerview

--- app/src/main/java/tech/henriquedev/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/tech/henriquedev/todoapp/fragments/list/ListFragment.kt
@@ -3,27 +3,44 @@ package tech.henriquedev.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import tech.henriquedev.todoapp.R
+import tech.henriquedev.todoapp.data.viewmodel.TodoViewModel
 import tech.henriquedev.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    private val mTodoViewModel: TodoViewModel by viewModels()
+
+    private val listAdapter: ListAdapter by lazy { ListAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Set Menu
-        setHasOptionsMenu(true)
-
         _binding = FragmentListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Set Menu
+        setHasOptionsMenu(true)
+
+        binding.recyclerView.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
+        }
+
+        mTodoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            listAdapter.setData(data)
+        })
 
         binding.fabCreate.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
