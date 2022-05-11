bb6c6559d0b725a16192c9a875d787cd8669fe7f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-10 22:01:04 -0300

    sort by high and low priority

--- app/src/main/java/tech/henriquedev/todoapp/data/TodoDao.kt
+++ app/src/main/java/tech/henriquedev/todoapp/data/TodoDao.kt
@@ -24,4 +24,10 @@ interface TodoDao {
 
     @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery")
     fun searchDatabase(searchQuery: String): LiveData<List<TodoData>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'H%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'L%' THEN 3 END")
+    fun sortByHighPriority(): LiveData<List<TodoData>>
+
+    @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+    fun sortByLowPriority(): LiveData<List<TodoData>>
 }
